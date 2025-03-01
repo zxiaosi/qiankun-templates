9770c2448aa74dea6420a6bf4e7e51e16b32eacd
zxiaosi <2692159310@qq.com>
2025-03-01 18:21:58 +0800

    链接主/子应用

--- packages/app1/.umirc.ts
+++ packages/app1/.umirc.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "umi";
+import { defineConfig } from 'umi';
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: '/', component: 'index' },
+    { path: '/docs', component: 'docs' },
   ],
   npmClient: 'npm',
+  plugins: ['@umijs/plugins/dist/qiankun'],
+  qiankun: {
+    slave: {},
+  },
 });
