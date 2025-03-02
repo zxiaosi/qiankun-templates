8df6a15522bfd70aa4a2aff8cb3c4339d60981ab
zxiaosi <2692159310@qq.com>
2025-03-02 22:21:05 +0800

    链接主/子应用

--- packages/app2/src/main.tsx
+++ packages/app2/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import {
+  QiankunProps,
+  qiankunWindow,
+  renderWithQiankun,
+} from 'vite-plugin-qiankun/dist/helper';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+/** 渲染函数 */
+const render = (container?: HTMLElement) => {
+  const app = container || (document.getElementById('root') as HTMLDivElement);
+  createRoot(app).render(<App />);
+};
+
+/** Qiankun 生命周期钩子 */
+const qiankun = () => {
+  renderWithQiankun({
+    bootstrap() {},
+    async mount(props: QiankunProps) {
+      render(props.container);
+    },
+    update: () => {},
+    unmount: () => {},
+  });
+};
+
+// 检查是否在 Qiankun 环境中
+console.log('qiankunWindow', qiankunWindow.__POWERED_BY_QIANKUN__);
+
+if (qiankunWindow.__POWERED_BY_QIANKUN__) qiankun(); // 以子应用的方式启动
+else render();
