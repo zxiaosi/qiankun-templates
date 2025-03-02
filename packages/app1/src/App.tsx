d036979a1354ea5f18eb3d739234d17be200ad48
zxiaosi <2692159310@qq.com>
2025-03-02 22:56:53 +0800

    多个子应用共存

--- packages/app1/src/App.tsx
+++ packages/app1/src/App.tsx
@@ -1,22 +1,41 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useMemo } from 'react';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  createMemoryRouter,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom';
 
 /** 路由前缀 */
 const basename = '/app1';
 
 /** 创建路由 */
-const routes = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <h2>app1</h2>,
-    },
-  ],
+const router: RouteObject[] = [
   {
-    basename,
-  }
-);
+    path: '/',
+    element: <h2>app1</h2>,
+  },
+];
 
-function App() {
+function App({ microProps }: any) {
+  const { routeType } = microProps;
+
+  const routes = useMemo(() => {
+    switch (routeType) {
+      case 'hash':
+        return createHashRouter(router, { basename });
+      case 'memory':
+        return createMemoryRouter(router, {
+          basename,
+          initialEntries: ['/app1'], // 初始化时指定初始路径, 用户可以通过浏览器前进后退操作
+          initialIndex: 0, // 初始化时指定初始索引
+        });
+      default:
+        return createBrowserRouter(router, { basename });
+    }
+  }, [routeType]);
+
   return <RouterProvider router={routes} />;
 }
 
